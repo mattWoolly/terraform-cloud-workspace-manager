69f65883f36f8e1b364b4abf5d9e88f639de459c
mattWoolly <matt@woolly.me>
2024-03-20 12:29:23 -0600

    refactor vcs trigger paths

--- terraform_cloud_workspace_manager.py
+++ terraform_cloud_workspace_manager.py
@@ -6,9 +6,9 @@ import argparse
 import os
 from pathlib import Path
 from urllib.parse import urljoin
-from colorama import Fore, Style, init
+from colorama import Fore, init
 
-# Initialize Colorama
+# Initialize Colorama for colored output in the terminal
 init(autoreset=True)
 
 # Constants and Configuration
@@ -19,68 +19,35 @@ HEADERS = {
     "Content-Type": "application/vnd.api+json",
 }
 
-
 class TerraformCloudAPI:
-    def __init__(self, base_url, headers):
-        self.base_url = base_url
-        self.headers = headers
+    def __init__(self):
+        self.base_url = API_BASE_URL
+        self.headers = HEADERS
 
     def patch_workspace(self, organization_name, workspace_name, data):
-        url = urljoin(
-            self.base_url,
-            f"organizations/{organization_name}/workspaces/{workspace_name}",
-        )
-        response = requests.patch(url, json=data, headers=self.headers)
-        return response
+        url = urljoin(self.base_url, f"organizations/{organization_name}/workspaces/{workspace_name}")
+        return requests.patch(url, json=data, headers=self.headers)
 
-
-class TerraformWorkspaceManager:
+class WorkspaceManager:
     def __init__(self, api_client):
         self.api_client = api_client
 
-    def find_workspace_and_org(self, filename="terraform.tf"):
+    @staticmethod
+    def find_workspace_and_org(filename="terraform.tf"):
         try:
             with open(filename, "r") as file:
                 content = file.read()
-            workspace_name = self._extract_value(
-                content, 'workspaces {[^}]*name = "(.*?)"'
-            )
-            organization_name = self._extract_value(content, 'organization = "(.*?)"')
+            workspace_name = WorkspaceManager._extract_value(content, 'workspaces {[^}]*name = "(.*?)"')
+            organization_name = WorkspaceManager._extract_value(content, 'organization = "(.*?)"')
             return workspace_name, organization_name
         except FileNotFoundError:
             print(Fore.RED + f"{filename} file not found in the current directory.\n")
             return None, None
 
-    def update_workspace_settings(
-        self, organization_name, workspace_name, mode=None, branch=None
-    ):
-        data = {"data": {"attributes": {}, "type": "workspaces"}}
-        if mode is not None:
-            data["data"]["attributes"]["execution-mode"] = (
-                "remote" if mode == "remote" else "local"
-            )
-        if branch is not None:
-            data["data"]["attributes"]["vcs-repo"] = {"branch": branch}
-        response = self.api_client.patch_workspace(
-            organization_name, workspace_name, data
-        )
-        operation = (
-            f"changed execution mode to {mode}"
-            if mode
-            else f"changed the VCS branch to {branch}"
-        )
-        self._log_response(response, operation + "\n")
-
-    def set_vcs_trigger_paths(self, organization_name, workspace_name, trigger_paths):
-        data = {"data": {"attributes": {"trigger-patterns": trigger_paths}}}
-        response = self.api_client.patch_workspace(
-            organization_name, workspace_name, data
-        )
-        self._log_response(response, "updated VCS trigger paths.\n")
-        if response.status_code == 200:
-            print(Fore.GREEN + "    Trigger Paths Set:\n")
-            for path in trigger_paths:
-                print(f"     - {path}\n")
+    def update_workspace_settings(self, organization_name, workspace_name, **kwargs):
+        data = {"data": {"attributes": kwargs, "type": "workspaces"}}
+        response = self.api_client.patch_workspace(organization_name, workspace_name, data)
+        WorkspaceManager._log_response(response, kwargs)
 
     @staticmethod
     def _extract_value(content, pattern):
@@ -88,77 +55,66 @@ class TerraformWorkspaceManager:
         return match.group(1) if match else None
 
     @staticmethod
-    def _log_response(response, operation):
+    def _log_response(response, settings):
         if response.status_code == 200:
-            print(Fore.GREEN + f"    Successfully {operation}")
+            if "working-directory" in settings:
+                print(Fore.GREEN + f"    ✓ Successfully updated working directory to: {settings['working-directory']}")
+            if "execution-mode" in settings:
+                print(Fore.GREEN + f"    ✓ Successfully updated workspace execution mode to: {settings['execution-mode']}")
+            if "trigger-patterns" in settings:
+                print(Fore.GREEN + f"    ✓ Successfully updated workspace trigger patterns to: {settings['trigger-patterns']}")
         else:
-            print(
-                Fore.RED
-                + f"    Failed to {operation}Status code: {response.status_code}, Message: {response.text}\n"
-            )
+            print(Fore.RED + f"    ✖ Failed to update workspace settings. Status code: {response.status_code}, Message: {response.text}\n")
 
+    @staticmethod
+    def find_repo_root(current_path):
+        path = Path(current_path)
+        for parent in [path] + list(path.parents):
+            if any((parent / '.git').exists() for child in parent.iterdir()):
+                return parent
+        return path
 
-def find_repo_root(current_path):
-    path = Path(current_path)
-    for parent in [path] + list(path.parents):
-        if any((parent / ".git").exists() for child in parent.iterdir()):
-            return parent
-    return path
-
+    @staticmethod
+    def get_working_directory():
+        repo_root = WorkspaceManager.find_repo_root(os.getcwd())
+        return os.path.relpath(os.getcwd(), start=repo_root)
 
 def main():
-    parser = argparse.ArgumentParser(
-        description="Manage Terraform Cloud workspace settings."
-    )
-    parser.add_argument(
-        "--local", action="store_true", help="Set workspace to local execution mode."
-    )
-    parser.add_argument(
-        "--remote", action="store_true", help="Set workspace to remote execution mode."
-    )
-    parser.add_argument(
-        "--change-branch", type=str, help="Change the VCS branch of the workspace."
-    )
-    parser.add_argument(
-        "--set-trigger-paths",
-        action="store_true",
-        help="Set VCS trigger paths based on the repo root.",
-    )
+    parser = argparse.ArgumentParser(description='Manage Terraform Cloud workspace settings.')
+    parser.add_argument('--local', action='store_true', help='Set workspace to local execution mode.')
+    parser.add_argument('--remote', action='store_true', help='Set workspace to remote execution mode.')
+    parser.add_argument('--change-branch', type=str, help='Change the VCS branch of the workspace.')
+    parser.add_argument('--set-working-directory', action='store_true', help='Update the working directory based on the current directory relative to the repo root.')
+    parser.add_argument('--set-trigger-paths', action='store_true', help='Set VCS trigger paths based on the current directory relative to the repo root.')
 
     args = parser.parse_args()
 
-    api_client = TerraformCloudAPI(API_BASE_URL, HEADERS)
-    manager = TerraformWorkspaceManager(api_client)
+    api_client = TerraformCloudAPI()
+    manager = WorkspaceManager(api_client)
 
     workspace_name, organization_name = manager.find_workspace_and_org()
 
-    if not workspace_name or not organization_name:
-        print(Fore.RED + "Workspace name or organization could not be found.\n")
-        return
+    if workspace_name and organization_name:
+        print(Fore.GREEN + f"Workspace '{workspace_name}' found in organization '{organization_name}'.")
 
-    print(
-        Fore.GREEN
-        + f"Workspace '{workspace_name}' found in organization '{organization_name}'.\n"
-    )
+        settings = {}
+        if args.local:
+            settings["execution-mode"] = "local"
+        elif args.remote:
+            settings["execution-mode"] = "remote"
+        if args.change_branch:
+            settings["vcs-repo"] = {"branch": args.change_branch}
+        if args.set_working_directory:
+            settings["working-directory"] = WorkspaceManager.get_working_directory()
+        if args.set_trigger_paths:
+            repo_root = WorkspaceManager.find_repo_root(os.getcwd())
+            relative_path = os.path.relpath(os.getcwd(), start=repo_root)
+            trigger_paths = [f"{relative_path}/**/*", f"{relative_path}/common/**/*"]
+            settings["trigger-patterns"] = trigger_paths
 
-    if args.local or args.remote:
-        mode = "local" if args.local else "remote"
-        manager.update_workspace_settings(organization_name, workspace_name, mode=mode)
-
-    if args.change_branch:
-        manager.update_workspace_settings(
-            organization_name, workspace_name, branch=args.change_branch
-        )
-
-    if args.set_trigger_paths:
-        repo_root = find_repo_root(os.getcwd())
-        relative_path = os.path.relpath(os.getcwd(), start=repo_root)
-        trigger_paths = [
-            f"{relative_path}/**/*",
-            f"{'/'.join(relative_path.split('/')[:-2])}/common/**/*",
-        ]
-        manager.set_vcs_trigger_paths(organization_name, workspace_name, trigger_paths)
-
+        # Update the workspace with the collected settings
+        if settings:
+            manager.update_workspace_settings(organization_name, workspace_name, **settings)
 
 if __name__ == "__main__":
     main()
